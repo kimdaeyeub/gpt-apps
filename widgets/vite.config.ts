12529663e115be4f3e6a3a38ffd83f5adc5aa176
kimdaeyeub <kdy3915@naver.com>
2026-03-19 18:56:56 +0900

    Test KV

--- widgets/vite.config.ts
+++ widgets/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-const FULL_URL = "https://server.devflowkim.workers.dev/";
+const FULL_URL = "https://flashcard-server.devflowkim.workers.dev/";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
